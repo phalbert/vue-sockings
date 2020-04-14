469cead2207f711ed0dd7a064d7bb41bcf55a674
hpal <ahosea15@gmail.com>
2020-04-14 13:53:21 +0300

    added emiting events

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 ## Vue Sockings
 
 A simple app using Vue as a script
+
+### Notes
+1. Load a child component `.js` file before the parent component
--- main.js
+++ main.js
@@ -1,36 +1,14 @@
+
+
 new Vue({
     el: "#app",
-    data() {
-        return {
-            brand: 'Appcellon',
-            product: 'Socks',
-            inventory: 1,
-            selectedVariant: 0,
-            details: ['80% cotton', '20% polyster', 'Nylon'],
-            cart: 0,
-            variants: [
-                { name: 'Blue', image: './assets/img/vmSocks-blue-onWhite.jpg', color: 'blue', quantity: 13 },
-                { name: 'Green', image: './assets/img/vmSocks-green-onWhite.jpg', color: 'green', quantity: 0 }
-            ]
-        };
+    data: {
+        cart: 0,
+        premium: false
     },
     methods: {
-        addToCart() {
+        updateCart() {
             this.cart += 1
-        },
-        updateProductImage(variantIndex) {
-            this.selectedVariant = variantIndex
-        }
-    },
-    computed: {
-        title() {
-            return `${this.brand} ${this.product}`
-        },
-        image() {
-            return this.variants[this.selectedVariant].image
-        },
-        inStock() {
-            return this.variants[this.selectedVariant].quantity
         }
     },
 });
